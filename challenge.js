bb6e49d92f6d1312eab78642ab4e714286c6addb
unknown <you@example.com>
2022-05-23 06:05:04 +0500

    fix

--- challenge.js
+++ challenge.js
@@ -1,17 +1,42 @@
 document.addEventListener("DOMContentLoaded", function(){
-  addWavyUnderline(findAllUnorderedListElements());
+
+
+
+
+  
+  addWavyUnderline(findAllUnorderedListElements("ul"));
+  displayProjects()
 });
 
 function addWavyUnderline(elements) {
+  for(let i = 0; i < elements.length; i++){
+    elements[i].style.textDecoration="wavy underline"
+  }
+    
 }
 
 function displayProjects() {
+  let div = document.getElementById(projects)
+  div.innerHTML=pareproject("haadii Projects","Ali","iam haadii and iam a web developer to be INSHALLAH and it will done hope so in 7 months or may be long but i dont want do that")
 }
 
 function findAllUnorderedListElements() {
+  results = document.querySelectorAll("social-media-links")
+  return results;
 }
 
-function prepareProjects() {
+function prepareProjects(firstProject, secondProject, thirdProject) {
+  let results = ""
+  let list = [firstProject,secondProject,thirdProject]
+  list.forEach((value,index) =>{
+    if(value.length > 20){
+      results += "<p>"+value+"</P><br>"
+    } else{
+      results += "<span>"+value+"</span><br>"
+      
+    }
+  })
+  return results;
 }
 
 exports.findAllUnorderedListElements = findAllUnorderedListElements;
